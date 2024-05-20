c6cb57606042efb2454b25a79794226838fff7f9
Marc Mansour <marcm9387@gmail.com>
2024-05-20 10:47:16 +0200

    adding validations in models and changing root to places#index

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,16 @@
 class Place < ApplicationRecord
   belongs_to :user
+
+  validates :address_country, presence: true
+  validates :address_city, presence: true
+  validates :address_street_name, presence: true
+  validates :address_zip_code, presence: true, numericality: { only_integer: true }
+  validates :screen_number, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
+  validates :desk_number, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
+  validates :bed_number, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
+  validates :internet_speed, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
+  validates :outdoor, inclusion: { in: [true, false] }
+  validates :air_conditionning, inclusion: { in: [true, false] }
+  validates :price_per_day, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :user_id, presence: true
 end
