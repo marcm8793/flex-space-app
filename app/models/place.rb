b792e58f812b61968054971b6a1ddedaf20d9dad
Marc Mansour <marcm9387@gmail.com>
2024-05-21 13:54:43 +0200

    adding presence: true in place.rb model

--- app/models/place.rb
+++ app/models/place.rb
@@ -5,12 +5,12 @@ class Place < ApplicationRecord
   validates :address_city, presence: true
   validates :address_street_name, presence: true
   validates :address_zip_code, presence: true, numericality: { only_integer: true }
-  validates :screen_number, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
-  validates :desk_number, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
-  validates :bed_number, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
-  validates :internet_speed, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
-  validates :outdoor, inclusion: { in: [true, false] }
-  validates :air_conditionning, inclusion: { in: [true, false] }
+  validates :screen_number, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, presence: true
+  validates :desk_number, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, presence: true
+  validates :bed_number, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, presence: true
+  validates :internet_speed, numericality: { greater_than_or_equal_to: 0 }, presence: true
+  validates :outdoor, inclusion: { in: [true, false] }, presence: true
+  validates :air_conditionning, inclusion: { in: [true, false] }, presence: true
   validates :price_per_day, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :user_id, presence: true
 end
