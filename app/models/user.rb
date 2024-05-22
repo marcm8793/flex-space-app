aa9f4d2ffc7cb78699d044dda5f9b0eb2a7338c9
Marc Mansour <marcm9387@gmail.com>
2024-05-22 20:55:57 +0200

    firstName and lastName/navbar/validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
