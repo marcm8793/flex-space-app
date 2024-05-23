b75b1298d4d4cd1af85405248479107bb337c736
Lucas Dimarellis <dimarellisl@gmail.com>
2024-05-23 20:40:52 +0200

    creation route trip

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,12 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :places, only: [:show, :index, :new, :create] do
-    resources :bookings, only: [:index, :create, :show]
+  resources :places do
+    resources :bookings
+  end
+
+  authenticated :user do
+    get '/trip', to: 'trip#index'
   end
 
   # Defines the root path route ("/")
