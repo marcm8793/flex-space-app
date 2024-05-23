27609b0be7f058cc4c58b97581c4c5ef1f34e9b2
Marc Mansour <marcm9387@gmail.com>
2024-05-23 23:56:48 +0200

    added CRUD controllers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,6 @@
 class BookingsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_place, only: [:new, :create]
+  before_action :set_place, only: [:edit, :update, :destroy]
 
   def new
     @booking = Booking.new
@@ -17,10 +17,30 @@ class BookingsController < ApplicationController
     end
   end
 
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    if @booking.update(booking_params)
+      redirect_to places_path, notice: "Booking updated successfully"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @place = current_user.places.find_by(id: params[:place_id])
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to places_path, notice: "Booking deleted successfully delete"
+  end
+
   private
 
   def booking_params
-    params.require(:booking).permit(:first_day, :last_day)
+    params.require(:booking).permit!
   end
 
   def set_place
