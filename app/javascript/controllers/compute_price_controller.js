563c983068940e9845cbe69fe8935858b9d96ef8
Victor Cariou <victorcariou0@gmail.com>
2024-05-25 16:22:19 +0200

    go to gouter

---
+++ app/javascript/controllers/compute_price_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="compute-price"
+export default class extends Controller {
+  static targets = ["firstDay", "lastDay", "priceElement", "totalDays", "totalPrice"]
+  static values = { fee: Number }
+
+  compute(){
+    const startDate = this.firstDayTarget.value;
+    const endDate = this.lastDayTarget.value;
+
+    let dateDiffInMilliseconds = new Date(endDate) - new Date(startDate);
+    let nbrOfNights = dateDiffInMilliseconds / 86400000;
+
+    if(startDate && endDate) {
+      this.totalDaysTarget.innerText = nbrOfNights
+      this.totalPriceTarget.innerText = this.feeValue * nbrOfNights
+    }
+  }
+}
