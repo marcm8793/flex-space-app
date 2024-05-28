95a34a5968945422862ed934e195ea9e8ac37b5c
Marc Mansour <marcm9387@gmail.com>
2024-05-28 22:57:24 +0200

    ajout wysiwig

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_28_174306) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_28_201545) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -78,6 +78,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_28_174306) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
     t.index ["user_id"], name: "index_places_on_user_id"
   end
 
